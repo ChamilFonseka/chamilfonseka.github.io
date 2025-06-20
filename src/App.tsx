261eb72174890684a0927afa064839734aa01e1e
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-06-20 11:52:53 +0530

    Install tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
 function App() {
   return (
     <>
-      <h1>Chamil Fonseka</h1>
+      <h1 className="
+        text-5xl font-bold 
+        bg-gradient-to-r from-sky-500 via-fuchsia-500 to-pink-500 
+        bg-clip-text text-transparent w-fit mx-auto"
+      >Chamil Fonseka</h1>
     </>
   )
 }
