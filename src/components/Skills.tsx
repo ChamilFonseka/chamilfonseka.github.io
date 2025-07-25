967e45fc3457ececf0d2e0aeda67e43739626bd8
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-07-25 13:56:38 +0530

    Added skills as bullet points

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -2,8 +2,23 @@ const Skills = () => {
     return (
         <section>
             <h2 className="text-2xl md:text-3xl xl:text-4xl tracking-tight font-semibold mb-4">Skills</h2>
-            <div className="flex flex-wrap gap-2 justify-center">
-            </div>
+                <ul className="list-disc px-4 md:px-8 lg:px-16">
+                    <li>Proficient in multiple programming languages, including <strong>Java</strong>, <strong>Kotlin</strong>, <strong>JavaScript</strong>, and <strong>TypeScript</strong>, with a strong understanding of <strong>object-oriented programming</strong> principles and <strong>design patterns</strong>.</li>
+                    <li>Extensive experience in developing scalable and high-performance software solutions, leveraging frameworks and technologies such as <strong>Spring Boot</strong>, <strong>Spring Cloud</strong>, <strong>Hibernate</strong>, <strong>Gradle</strong> and <strong>Maven</strong>.</li>
+                    <li>Experienced in full-stack development, including front-end technologies like <strong>Angular</strong>, <strong>React</strong>, <strong>HTML</strong>, <strong>CSS</strong>, and <strong>jQuery</strong>, as well as back-end technologies such as <strong>Node.js</strong>.</li>
+                    <li>Proficient in database design and optimization, including experience in writing efficient <strong>SQL</strong> queries for relational databases such as <strong>PostgreSQL</strong>, <strong>Oracle</strong>, <strong>MS SQL Server</strong>, and <strong>MySQL</strong>.</li>
+                    <li>Familiar with <strong>NoSQL</strong> databases and their principles, with a working knowledge of data modeling for popular NoSQL databases such as <strong>MongoDB</strong> and <strong>DynamoDB</strong>.</li>
+                    <li>Solid experience in designing and implementing <strong>RESTful APIs</strong>, <strong>microservices</strong> architectures, and <strong>cloud-based</strong> solutions.</li>
+                    <li>Deep understanding of software development lifecycle (<strong>SDLC</strong>) methodologies, including <strong>Agile</strong> and <strong>Scrum</strong>.</li>
+                    <li>Practical experience working with various <strong>AWS</strong> services, encompassing <strong>EKS</strong> (Elastic Kubernetes Service), <strong>ECS</strong> (Elastic Container Service), <strong>RDS</strong> (Relational Database Service), <strong>DynamoDB</strong>, <strong>Lambda</strong>, <strong>Route53</strong>, <strong>Cognito</strong>, <strong>CodeBuild</strong>, <strong>CodePipeline</strong>, <strong>CloudWatch</strong>, <strong>API Gateway</strong>, <strong>EC2</strong> (Elastic Compute Cloud), and <strong>S3</strong> (Simple Storage Service).</li>
+                    <li>Proficient in the utilization of messaging platforms such as <strong>SQS</strong> (Simple Queue Service) and <strong>ActiveMQ</strong>.</li>
+                    <li>Exposure to Infrastructure as Code (<strong>IaC</strong>) tools, particularly with <strong>AWS CDK</strong> (Cloud Development Kit) and <strong>CloudFormation</strong>.</li>
+                    <li>Excellent knowledge in using version control systems such as <strong>GitLab</strong>, <strong>AWS CodeCommit</strong> and <strong>GitHub</strong>.</li>
+                    <li>Hands-on experience in configuring <strong>CI/CD</strong> pipelines using <strong>Jenkins</strong>, <strong>GitLab CI/CD</strong> and <strong>AWS CodePipeline</strong>.</li>
+                    <li>Competent in containerization technologies such as <strong>Docker</strong> and orchestration frameworks like <strong>Kubernetes</strong>.</li>
+                    <li>Proficient in applying <strong>Test-Driven Development</strong> (<strong>TDD</strong>) principles and practices, with hands-on experience in writing comprehensive unit tests and integration tests using <strong>JUnit</strong>, <strong>Mockito</strong>, and <strong>TestContainers</strong>.</li>
+                    <li>Possessing strong problem-solving and analytical skills, with a proven ability to promptly identify and address complex technical challenges while effectively applying software <strong>design patterns</strong> and principles.</li>
+                </ul>
             <hr className="my-4" />
         </section>
     );
