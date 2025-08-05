98ff6863a978103aad06774aa7edebb50859fd72
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-08-05 19:29:03 +0530

    Added education  as bullet points

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -2,6 +2,11 @@ const Education = () => {
     return (
         <section>
             <h1>Education</h1>
+            <ul className="list-disc px-4 md:px-8 lg:px-16">
+                <li>University College Dublin | Bachelor of Science (Honours Degree) in Computer Science | 2012–2014.</li>
+                <li>National Institute of Business Management | Higher Diploma in Computer Based Information Systems | 2009–2010.</li>
+                <li>National Institute of Business Management | Diploma in Computer System Design | 2008–2009.</li>
+            </ul>
             <hr className="my-4" />
         </section>
     );
