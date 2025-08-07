1be7d8d9ca8676e4aaf428cbc1c78ef144e66328
Chamil Fonseka <fonseka.chamil@gmail.com>
2025-08-07 21:07:14 +0530

    Added certifications as bullet points

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -3,6 +3,9 @@ const Certifications = () => {
         <section>
             <h1>Certifications</h1>
             <hr className="my-4" />
+            <ul className="list-disc px-4 md:px-8 lg:px-16">
+                <li>AWS certified solutions architect - Associate (2022).</li>
+            </ul>
         </section>
     );
 };
